3662aa523e08a019104ae231007f59715cf0b9fe
adminblackjack <91126509+adminblackjack@users.noreply.github.com>
2021-11-17 16:42:20 +0800

    Add files via upload

---
+++ get_data/getdata.py
@@ -0,0 +1,48 @@
+import rqdatac
+import os
+import csv
+import pandas as pd
+import time
+
+rqdatac.init()
+
+#print(rqdatac.user.get_quota())
+
+#value=rqdatac.get_price('000001.XSHE',start_date=20160301,end_date=20200301)
+#value.to_csv('000001.csv')
+#print(rqdatac.user.get_quota())
+'''
+path='C:/Users/Administrator/rq/'
+os.makedirs(path+"2")
+fo=open("begin.in",mode="wb")
+fo.close()
+'''
+path=os.getcwd()
+L=list(path)
+for i in range(0,len(L)):
+    if (L[i]=="\\"):
+        L[i]='/'
+path=''.join(L)
+os.chdir(path+"/all_stocks")
+#os.makedirs(path+"/1")
+all_stocks=pd.read_csv("all_stocks.csv",encoding="GB18030")
+N=None
+cnt=0
+length=len(all_stocks['id'])
+for i in range(0,length):
+    if (all_stocks['end_date'][i]<="2016-03-01"):
+        continue
+    index=all_stocks['id'][i]
+    if ('600848') in index:
+        cnt+=1
+        continue
+    if (cnt==0):
+        continue
+    value=rqdatac.get_price(index,start_date=20160301,end_date=20191231)
+    print(type(value))
+    if (type(value)==type(N)):
+        continue
+    os.chdir(path)
+    os.makedirs(path+"/"+index)
+    os.chdir(path+"/"+index)
+    value.to_csv(index+".csv")
